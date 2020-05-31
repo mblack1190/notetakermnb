db821bebb308b74d50b8d6b5078051183032c2a8
Meredith Black <mblack1190@gmail.com>
2020-05-30 19:05:59 -0500

    set up first route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req, res) => {
+  res.json('db');
+});
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
